76be782666266c488e375a02a146b3e619ce3be3
KirillPogranichny <kirillpogranichny@gmail.com>
2023-06-12 20:06:35 +1000

    fix functions and done first modification of power method

--- min_eigval_modification.py
+++ min_eigval_modification.py
@@ -4,15 +4,16 @@ import numpy as np
 def power_method():
     ctr = 1
     x_prev = x_0
-    x_next = A @ x_prev / np.linalg.norm(A @ x_prev)
+    x_next = A_inv @ x_prev / np.linalg.norm(A_inv @ x_prev)
     print(f"Итерация {ctr}:\n x_{ctr}:\n {x_next}")
     lam_prev, lam = 0, 0
 
     while True:
         ctr += 1
         x_prev = x_next
-        x_next = A @ x_prev / np.linalg.norm(A @ x_prev)
-        lam = (x_next.T @ A @ x_next) / (x_next.T @ x_next)
+        x_next = A_inv @ x_prev / np.linalg.norm(A_inv @ x_prev)
+
+        lam = (x_next.T @ A_inv @ x_next) / (x_next.T @ x_next)
 
         if np.abs(lam - lam_prev) < eps:
             print(f"На {ctr} итерации условие |lam - lam_prev| < eps перестает выполняться")
@@ -24,7 +25,7 @@ def power_method():
         lam_prev = lam
         print(f"Итерация {ctr}:\n x_{ctr}:\n {x_next}")
 
-    return ctr-1, x_prev, lam_prev
+    return ctr-1, x_prev, 1/lam_prev
 
 
 if __name__ == "__main__":
@@ -36,21 +37,23 @@ if __name__ == "__main__":
         [9, 11, 5, 3, 1, 2],
         [9, 14, 17, 8, 15, 3]
     ])
-    x_0 = np.array([[5, 1, 10, 5, 12, 6]]).T
+    x_0 = np.array([[1, 1, 1, 1, 1, 1]]).T
     eps = 1e-6
+    A_inv = np.linalg.inv(A)
     print("A:\n", A)
+    print("A_inv:\n", A_inv)
     print("x_0:\n", x_0, "\n")
 
     ctr, x, lam = power_method()
     if ctr % 10 == 2:
         print(f"\nДля нашего набора данных потребовалась {ctr} итерация\n"
               f"Собственный вектор x_{ctr}:\n {x}\n"
-              f"Максимальное по модулю собственное значение: {lam[0][0]}")
+              f"Минимальное по модулю собственное значение: {lam[0][0]}")
     elif 2 < ctr % 10 < 6:
         print(f"\nДля нашего набора данных потребовалось {ctr} итерации\n"
               f"Собственный вектор x_{ctr}:\n {x}\n"
-              f"Максимальное по модулю собственное значение: {lam[0][0]}")
+              f"Минимальное по модулю собственное значение: {lam[0][0]}")
     else:
         print(f"\nДля нашего набора данных потребовалось {ctr} итераций\n"
               f"Собственный вектор x_{ctr}:\n {x}\n"
-              f"Максимальное по модулю собственное значение: {lam[0][0]}")
+              f"Минимальное по модулю собственное значение: {lam[0][0]}")
